23210e64a5c649bd73f90bb69a2264aaf79707eb
ruthearle <rrearle@gmail.com>
2014-09-16 23:14:23 +0100

    changed file structure

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-  var p1 = new Player('Ruth');
-  var p2 = new Player('Kevin');
-  var game = new Game(p1, p2);
-  var choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
-  $('.choices img').on('click', function() {
+    var p1 = new Player('Ruth');
+    var p2 = new Player('The Matrix');
+    var game = new Game(p1, p2);
+    var choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+    $('.choices img').on('click', function() {
     p1.picks($(this).data('pick'));
     p2.picks(choices[Math.floor(Math.random()*choices.length)]);
 
     $('<li>' + game.winningMessage() + '</li>').prependTo('#results').slideDown();
 
     $('#results li:gt(3)').fadeOut(function(){
-      $(this).remove();
+    $(this).remove();
     });
-  })
-})
+  });
+});
